d26f42847804489d402de1e4d96cdf9d0b9ecf09
einFreak <felix0609@web.de>
2021-02-17 23:38:28 +0100

    added currentLocation and getPermission

--- app/src/main/java/com/felixwild/fahnenklauen/ui/map/MapFragment.kt
+++ app/src/main/java/com/felixwild/fahnenklauen/ui/map/MapFragment.kt
@@ -1,24 +1,44 @@
 package com.felixwild.fahnenklauen.ui.map
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.felixwild.fahnenklauen.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnSuccessListener
 
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapViewModel: MapViewModel
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val TAG = "MapFragment"
+    private var locationPermission = false
+    private var currentLocation: Location = Location("")
+    private var current = LatLng(0.0, 0.0)
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -29,12 +49,40 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 ViewModelProvider(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_map, container, false)
 
+        val textView: TextView = root.findViewById(R.id.text_map)
+        mapViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
 
         mapFragment?.getMapAsync(this)
+        Log.d(TAG, "Trying to get Perms")
+        setupPermissions()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
         return root
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d(TAG, "value fusedLocCLient: $fusedLocationClient")
+        var location: Location
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    Log.d(TAG, "Location returned: $location")
+                    currentLocation = location
+                    // write code here to to make the API call to the weather service and update the UI
+                    // this code here is running on the Main UI thread as far as I understand
+                } else {
+                    Log.d(TAG, "Null location returned.")
+                }
+            }
     }
 
     override fun onMapReady(p0: GoogleMap?) {
@@ -44,7 +92,51 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
+        current = LatLng(currentLocation.latitude, currentLocation.longitude)
+        mMap.addMarker(MarkerOptions().position(current).title("current Location"))
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current))
+    }
+
+    private fun setupPermissions() {
+        val requestPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                isGranted: Boolean ->
+            Log.d(TAG, "isGranted: $isGranted")
+            if(isGranted) {
+                Toast.makeText(this.context, "Location Permission given", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this.context, "No Permission - setting to default", Toast.LENGTH_SHORT).show()
+                currentLocation = Location("")
+                currentLocation.longitude = 0.0
+                currentLocation.latitude = 0.0
+            }
+        }
+        Log.d(TAG, "requestPerm created")
+
+        Log.d(TAG, "Check for Perm")
+        when {
+            ContextCompat.checkSelfPermission(
+                this.requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.d(TAG, "Permission granted")
+                // You can use the API that requires the permission.
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            //showInContextUI(...)
+            Toast.makeText(this.context, "No Permission - setting to default2", Toast.LENGTH_SHORT).show()
+        }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermission.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
     }
 }
+
